a7ed70c65176cf13e0b7f567e9cfa4781dd0041a
chenshenghui <chenshenghui@360.cn>
2018-06-26 14:49:37 +0800

    新建项目

---
+++ src/main/java/kaihu/kaihu/App.java
@@ -0,0 +1,13 @@
+package kaihu.kaihu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
